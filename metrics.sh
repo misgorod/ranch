371e259d1380146fae33bcee4d92b80c9e13a04c
Andrey Bulgaru <agbulgaru@avito.ru>
2020-05-09 20:41:48 +0300

    parse metrics

--- metrics.sh
+++ metrics.sh
@@ -3,38 +3,66 @@
 servers="testing-kafka-dev01:9092,testing-kafka-dev02:9092,testing-kafka-dev03:9092"
 topic="test"
 
-declare -A pairs=( [512000]=1024 [4096000]=128 [51200]=10240 [500]=1048576 )
+declare -A pairs=( [512000]=1024 [4096000]=128 )
+#declare -A pairs=( [100]=1024 )
 
-echo "records,size,acks,compression,inflight,batch,linger,time,batch-size-avg,batch-size-max,batch-split-rate,batch-split-total,buffer-available-bytes,buffer-exhausted-rate,buffer-exhausted-total,buffer-total-bytes,bufferpool-wait-ratio,bufferpool-wait-time-total,compression-rate-avg,connection-close-rate,connection-close-total,connection-count,connection-creation-rate,connection-creation-total,failed-authentication-rate,failed-authentication-total,failed-reauthentication-rate,failed-reauthentication-total,incoming-byte-rate,incoming-byte-total,io-ratio,io-time-ns-avg,io-wait-ratio,io-wait-time-ns-avg,io-waittime-total,iotime-total,metadata-age,network-io-rate,network-io-total,outgoing-byte-rate,outgoing-byte-total,produce-throttle-time-avg,produce-throttle-time-max,reauthentication-latency-avg,reauthentication-latency-max,record-error-rate,record-error-total,record-queue-time-avg,record-queue-time-max,record-retry-rate,record-retry-total,record-send-rate,record-send-total,record-size-avg,record-size-max,records-per-request-avg,request-latency-avg,request-latency-max,request-rate,request-size-avg,request-size-max,request-total,requests-in-flight,response-rate,response-total,select-rate,select-total,successful-authentication-no-reauth-total,successful-authentication-rate,successful-authentication-total,successful-reauthentication-rate,successful-reauthentication-total,waiting-threads,incoming-byte-rate--2,incoming-byte-rate-1,incoming-byte-rate-2,incoming-byte-rate-3,incoming-byte-total--2,incoming-byte-total-1,incoming-byte-total-2,incoming-byte-total-3,outgoing-byte-rate--2,outgoing-byte-rate-1,outgoing-byte-rate-2,outgoing-byte-rate-3,outgoing-byte-total--2,outgoing-byte-total-1,outgoing-byte-total-2,outgoing-byte-total-3,request-latency-avg--2,request-latency-avg-1,request-latency-avg-2,request-latency-avg-3,request-latency-max--2,request-latency-max-1,request-latency-max-2,request-latency-max-3,request-rate--2,request-rate-1,request-rate-2,request-rate-3,request-size-avg--2,request-size-avg-1,request-size-avg-2,request-size-avg-3,request-size-max--2,request-size-max-1,request-size-max-2,request-size-max-3,request-total--2,request-total-1,request-total-2,request-total-3,response-rate--2,response-rate-1,response-rate-2,response-rate-3,response-total--2,response-total-1,response-total-2,response-total-3,byte-rate,byte-total,compression-rate,record-error-rate,record-error-total,record-retry-rate,record-retry-total,record-send-rate,record-send-total"
+#echo "records,size,acks,compression,inflight,batch,linger,time,batch-size-avg,batch-size-max,batch-split-rate,batch-split-total,buffer-available-bytes,buffer-exhausted-rate,buffer-exhausted-total,buffer-total-bytes,bufferpool-wait-ratio,bufferpool-wait-time-total,compression-rate-avg,connection-close-rate,connection-close-total,connection-count,connection-creation-rate,connection-creation-total,failed-authentication-rate,failed-authentication-total,failed-reauthentication-rate,failed-reauthentication-total,incoming-byte-rate,incoming-byte-total,io-ratio,io-time-ns-avg,io-wait-ratio,io-wait-time-ns-avg,io-waittime-total,iotime-total,metadata-age,network-io-rate,network-io-total,outgoing-byte-rate,outgoing-byte-total,produce-throttle-time-avg,produce-throttle-time-max,reauthentication-latency-avg,reauthentication-latency-max,record-error-rate,record-error-total,record-queue-time-avg,record-queue-time-max,record-retry-rate,record-retry-total,record-send-rate,record-send-total,record-size-avg,record-size-max,records-per-request-avg,request-latency-avg,request-latency-max,request-rate,request-size-avg,request-size-max,request-total,requests-in-flight,response-rate,response-total,select-rate,select-total,successful-authentication-no-reauth-total,successful-authentication-rate,successful-authentication-total,successful-reauthentication-rate,successful-reauthentication-total,waiting-threads,node-incoming-byte-rate--3,node-incoming-byte-rate-1,node-incoming-byte-rate-2,node-incoming-byte-rate-3,node-incoming-byte-total--3,node-incoming-byte-total-1,node-incoming-byte-total-2,node-incoming-byte-total-3,node-outgoing-byte-rate--3,node-outgoing-byte-rate-1,node-outgoing-byte-rate-2,node-outgoing-byte-rate-3,node-outgoing-byte-total--3,node-outgoing-byte-total-1,node-outgoing-byte-total-2,node-outgoing-byte-total-3,node-request-latency-avg--3,node-request-latency-avg-1,node-request-latency-avg-2,node-request-latency-avg-3,node-request-latency-max--3,node-request-latency-max-1,node-request-latency-max-2,node-request-latency-max-3,node-request-rate--3,node-request-rate-1,node-request-rate-2,node-request-rate-3,node-request-size-avg--3,node-request-size-avg-1,node-request-size-avg-2,node-request-size-avg-3,node-request-size-max--3,node-request-size-max-1,node-request-size-max-2,node-request-size-max-3,node-request-total--3,node-request-total-1,node-request-total-2,node-request-total-3,node-response-rate--3,node-response-rate-1,node-response-rate-2,node-response-rate-3,node-response-total--3,node-response-total-1,node-response-total-2,node-response-total-3,topic-byte-rate,topic-byte-total,topic-compression-rate,topic-record-error-rate,topic-record-error-total,topic-record-retry-rate,topic-record-retry-total,topic-record-send-rate,topic-record-send-total"
 
 for records in "${!pairs[@]}"; do
     for acks in -1 0 1; do
         for compression in "snappy" "none"; do
             for inflight in 1 5 10; do
-                for batch in 0 1048576 9437184; do
-                    for linger in 0 20 1000; do
-                        start=$(date +%s)
-                        metrics=$(kafka-producer-perf-test --topic ${topic} --num-records ${records} --record-size ${pairs[$records]} --throughput -1 --print-metrics --producer-props bootstrap.servers=${servers} client.id=test acks=${acks} compression.type=${compression} max.in.flight.requests.per.connection=${inflight} batch.size=${batch} linger.ms=${linger}) 
-                        time=$(($(date +%s) - ${start}))
+                for batch in 0; do
+                    for linger in 0 20 150 1000; do
+                        ncout="$(nc -zv testing-kafka-dev01 9092 2>&1 && nc -zv testing-kafka-dev02 9092 2>&1 && nc -zv testing-kafka-dev03 9092 2>&1)"
+                        if [[ $? != 0 ]]; then
+                            echo "$records,${pairs[$records]},$acks,\"$compression\",$inflight,$batch,$linger,BROKER BROKEN: ${ncout}"
+                            echo
+                            continue
+                        fi
+
+                        (kafka-leader-election --bootstrap-server testing-kafka-dev01:9092,testing-kafka-dev02:9092,testing-kafka-dev03:9092 --election-type preferred --all-topic-partitions)
                         
-                        echo -n "$records,${pairs[$records]},$acks,\"$compression\",$inflight,$batch,$linger,$time"
+                        start=$(date +%s%3N)
+
+                        metrics="$(kafka-producer-perf-test --topic ${topic} --num-records ${records} --record-size ${pairs[$records]} --throughput -1 --print-metrics --producer-props bootstrap.servers=${servers} client.id=test acks=${acks} compression.type=${compression} max.in.flight.requests.per.connection=${inflight} batch.size=${batch} linger.ms=${linger})"
+                        if [[ $? == 124 ]]; then
+                            echo "$records,${pairs[$records]},$acks,\"$compression\",$inflight,$batch,$linger,TIMEOUT"
+                            echo
+                            continue
+                        fi
+
+                        time=$(($(date +%s%3N) - ${start}))
+                        
+                        echo "$records,${pairs[$records]},$acks,\"$compression\",$inflight,$batch,$linger,$time"
+                        #echo "$metrics"
 
                         while IFS= read -r metric; do 
+                            regex="${records} records sent.*"
+                            if [[ "${metric}" =~ $regex ]]; then
+                                echo "$metric"
+                            fi
                             regex='producer-metrics:(.*):\{.*\}\s+: (.*)'
                             if [[ "${metric}" =~ $regex ]]; then
-                                echo -n ",${BASH_REMATCH[2]}"
+                                echo "$metric"
+                                #echo -n ",${BASH_REMATCH[2]}"
+                                #echo -n ",${BASH_REMATCH[1]}"
                             fi
                             regex='producer-node-metrics:(.*):\{.*node-id=node-(.*)\}\s+: (.*)'
                             if [[ "${metric}" =~ $regex ]]; then
-                                echo -n ",${BASH_REMATCH[3]}"
+                                echo "$metric"
+                                #echo -n ",${BASH_REMATCH[3]}"
+                                #echo -n ",node-${BASH_REMATCH[1]}-${BASH_REMATCH[2]}"
                             fi
                             regex='producer-topic-metrics:(.*):\{.*\}\s+: (.*)'
                             if [[ "${metric}" =~ $regex ]]; then
-                                echo -n ",${BASH_REMATCH[2]}"
+                                echo "$metric"
+                                #echo -n ",${BASH_REMATCH[2]}"
+                                #echo -n ",topic-${BASH_REMATCH[1]}"
                             fi
                         done <<< "$metrics"
-                        echo
 
+                        sleep 3
                     done
                 done
             done
---
+++ parse_metrics.py
@@ -0,0 +1,148 @@
+import json
+import re
+
+
+def delete_keys(m, keys):
+    for key in keys:
+        if key in m:
+            del m[key]
+    return m
+
+
+def try_float(s):
+    try:
+        f = float(s)
+        return f
+    except:
+        return s
+
+
+def parse_header(metric, line):
+    match = re.match(r'(\d+),(\d+),([\d\-]+),"(\w+)",(\d+),(\d+),(\d+),(\d+)', line)
+    if not match:
+        raise Exception(f"header not matched: {line}")
+
+    metric['records'] = try_float(match.group(1))
+    metric['size_b'] = try_float(match.group(2))
+    metric['acks'] = match.group(3)
+    metric['compression'] = match.group(4)
+    metric['inflight'] = try_float(match.group(5))
+    metric['batch_b'] = try_float(match.group(6))
+    metric['linger_ms'] = try_float(match.group(7))
+    metric['time_ms'] = try_float(match.group(8))
+
+    return metric
+
+
+def parse_metric_result(metric, line):
+    match = re.match(r'\d+ records sent, ([\d.]+) records/sec \(([\d.]+) MB/sec\), ([\d.]+) ms avg latency, ([\d.]+) ms max latency, (\d+) ms 50th, (\d+) ms 95th, (\d+) ms 99th, (\d+) ms 99.9th.', line)
+    if not match:
+        raise Exception(f"metric result not matched: {line}")
+
+    metric['records_persec'] = try_float(match.group(1))
+    metric['mb_persec'] = try_float(match.group(2))
+    metric['latency_avg_ms'] = try_float(match.group(3))
+    metric['latency_max_ms'] = try_float(match.group(4))
+    metric['latency_50th_ms'] = try_float(match.group(5))
+    metric['latency_95th_ms'] = try_float(match.group(6))
+    metric['latency_99th_ms'] = try_float(match.group(7))
+    metric['latency_999th_ms'] = try_float(match.group(8))
+
+    return metric
+
+
+def parse_metric(metric, line):
+    if re.match(r'producer-node-metrics.*', line):
+        match = re.match(r'producer-node-metrics:([\w\-]+):\{client-id=test, node-id=node-([\d\-]+)\}\s+: (.*)',line)
+        if not match:
+            raise Exception(f'producer node metrics not matched: {line}')
+
+        metric[f"producer-node_{match.group(1)}_{match.group(2)}"] = try_float(match.group(3))
+    elif re.match(r'[\w\-]+-metrics.*', line):
+        match = re.match(r'([\w\-]+)-metrics:([\w\-]+):\{.*\}\s+: (.*)',line)
+        if not match:
+            raise Exception(f'producer node metrics not matched: {line}')
+
+        metric[f"{match.group(1)}_{match.group(2)}"] = try_float(match.group(3))
+    return metric
+
+
+def process_files(files):
+    metrics = []
+    metric = {}
+    for file in files:
+        with open(file, 'r+') as f:
+            for line in f:
+                line = line.strip()
+                if line == 'start':
+                    delete_keys(metric, [
+                        'producer-node_request-latency-avg_-1',
+                        'producer-node_request-latency-avg_-2',
+                        'producer-node_request-latency-avg_-3',
+                        'producer-node_incoming-byte-rate_-1',
+                        'producer-node_incoming-byte-rate_-2',
+                        'producer-node_incoming-byte-rate_-3',
+                        'producer-node_incoming-byte-total_-1',
+                        'producer-node_incoming-byte-total_-2',
+                        'producer-node_incoming-byte-total_-3',
+                        'producer-node_outgoing-byte-rate_-1',
+                        'producer-node_outgoing-byte-rate_-2',
+                        'producer-node_outgoing-byte-rate_-3',
+                        'producer-node_outgoing-byte-total_-1',
+                        'producer-node_outgoing-byte-total_-2',
+                        'producer-node_outgoing-byte-total_-3',
+                        'producer-node_request-latency-max_-1',
+                        'producer-node_request-latency-max_-2',
+                        'producer-node_request-latency-max_-3',
+                        'producer-node_request-rate_-1',
+                        'producer-node_request-rate_-2',
+                        'producer-node_request-rate_-3',
+                        'producer-node_request-size-avg_-1',
+                        'producer-node_request-size-avg_-2',
+                        'producer-node_request-size-avg_-3',
+                        'producer-node_request-size-max_-1',
+                        'producer-node_request-size-max_-2',
+                        'producer-node_request-size-max_-3',
+                        'producer-node_request-total_-1',
+                        'producer-node_request-total_-2',
+                        'producer-node_request-total_-3',
+                        'producer-node_response-rate_-1',
+                        'producer-node_response-rate_-2',
+                        'producer-node_response-rate_-3',
+                        'producer-node_response-total_-1',
+                        'producer-node_response-total_-2',
+                        'producer-node_response-total_-3',
+                        'producer_reauthentication-latency-avg',
+                        'producer_reauthentication-latency-max',
+                        'producer_successful-authentication-no-reauth-total',
+                        'producer_successful-authentication-rate',
+                        'producer_successful-authentication-total',
+                        'producer_successful-reauthentication-rate',
+                        'producer_successful-reauthentication-total',
+                        'producer_failed-authentication-rate',
+                        'producer_failed-authentication-total',
+                        'producer_failed-reauthentication-rate',
+                        'producer_failed-reauthentication-total',
+                    ])
+
+                    metrics.append(metric)
+                    metric = {}
+                elif re.match(r'.*,"snappy",.*', line):
+                    metric = parse_header(metric, line)
+                elif re.match(r'.*,"none",.*', line):
+                    metric = parse_header(metric, line)
+                elif re.match(r'.*records sent.*', line):
+                    metric = parse_metric_result(metric, line)
+                else:
+                    metric = parse_metric(metric, line)
+    return metrics
+
+
+def main():
+    metrics = process_files(['metrics-raw-500', 'metrics-raw-wo-timeout'])
+    
+    print(json.dumps(metrics).replace("NaN" , "null"))
+
+
+if __name__ == '__main__':
+    main()
